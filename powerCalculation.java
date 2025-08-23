cd95bb9c8b95421834a63527fa5028bca4861bba
abhibhardwaj8 <abhibhardwaj08@gmail.com>
2025-08-23 21:13:09 +0530

    Removed all files from branch

--- powerCalculation.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class powerCalculation {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int base = sc.nextInt();
-        int exponent = sc.nextInt();
-        System.out.println(Math.pow(base, exponent));
-    }
-}
