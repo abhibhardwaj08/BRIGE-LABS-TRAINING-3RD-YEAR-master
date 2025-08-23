aa7dcac6405f307f3efbbbf710250e57457fd8eb
abhibhardwaj8 <abhibhardwaj08@gmail.com>
2025-08-23 21:18:08 +0530

    Removed all files from branch

--- kmToMiles.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class kmToMiles {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        double km = sc.nextInt();
-        double miles = km*(0.621371);
-    }
-}
